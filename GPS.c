4195ecf1c80d2735a9981c8defcb944160970182
earth75 <alexis.tetar@gmail.com>
2015-11-15 18:37:39 +0100

    remove unneeded content

--- GPS.c
+++ GPS.c
@@ -4,39 +4,6 @@
 
 #include <sys\exception.h>				// system exception
 #include <cdefbf533.h>
-/*#include <drivers\adi_dev.h>			// device manager includes
-#include <drivers\uart\adi_uart.h>		// uart driver includes
-#include "ezkitutilities.h"				// EZ-Kit utilities*/
- 
-/* */
-
-/*#define UART_LCR 0xFFC0040C
-#define pUART_LCR (volatile unsigned int *) UART_LCR
-#define UART_MCR 0xFFC00410
-#define pUART_MCR (volatile unsigned int *) UART_MCR
-
-#define UART_LSR 0xFFC00414
-#define pUART_LSR (volatile unsigned int *) UART_LSR
-#define UART_THR 0xFFC00400
-#define pUART_THR (volatile unsigned int *) UART_THR
-#define UART_RBR 0xFFC00400
-#define pUART_RBR (volatile unsigned int *) UART_RBR
-#define UART_DLL 0xFFC00400
-#define pUART_DLL (volatile unsigned int *) UART_DLL
-#define UART_DLH 0xFFC00404
-#define pUART_DLH (volatile unsigned int *) UART_DLH
-#define UART_GCTL 0xFFC00424
-#define pUART_GCTL (volatile unsigned int *) UART_GCTL
-
-#define SIC_IMASK 0xFFC0010C
-#define pSIC_IMASK (volatile unsigned int *) SIC_IMASK
-#define UART_IER 0xFFC00404
-#define pUART_IER (volatile unsigned int *) UART_IER
-
-#define DMA7_CONFIG 0xFFC00DC8
-#define pDMA7_CONFIG (volatile unsigned int *) DMA7_CONFIG
-#define DMA7_X_COUNT 0xFFC00DD0
-#define pDMA7_X_COUNT (volatile unsigned int *) DMA7_X_COUNT*/
 
 typedef enum UARTstate {ATT, TSTG, TSTA, LOAD, RDY} UART_State;
 UART_State state = ATT;
